602f3123f3edb92311687431a018c1638a394392
Task Nagashige <task.19.xx@gmail.com>
2020-07-13 01:33:25 +0900

    ステータスのデフォルト値を設定した

--- migrations/2020071200000_CreateTodo.php
+++ migrations/2020071200000_CreateTodo.php
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS `todo` (
 `id` int(11) AUTO_INCREMENT,
 `title` varchar(255) NOT NULL,
 `due_date` DATE NOT NULL,
-`status` tinyint(1) NOT NULL,
+`status` tinyint(1) NOT NULL DEFAULT 0,
 `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 `deleted_at` DATETIME DEFAULT NULL,
 PRIMARY KEY (`id`)
