74fabdf22734d1bd06ec1f97664189de50d389f0
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-10-07 12:24:47 +0900

    画像変更

--- index.php
+++ index.php
@@ -67,6 +67,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             <thead>
             <tr>
                 <th>タイトル</th>
+                <th>画像</th>
                 <th>期限</th>
                 <th>状態</th>
                 <th>更新</th>
@@ -79,7 +80,17 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                 ?>
                 <tr>
                     <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
-                        <td><?=$todo['title']; ?></td>
+                        <td  style="width: 300px"><?=$todo['title']; ?></td>
+                        <td>
+                            <?php
+                            if (isset($todo['image'])) {
+                                $image = $todo['image'];
+                                echo "<img src='upload/$image'; width='100'/>";
+                            } else {
+                                echo "---";
+                            }
+                            ?>
+                        </td>
                         <td><?=$todo['due_date']; ?></td>
                         <td class="label">
                             <label>
